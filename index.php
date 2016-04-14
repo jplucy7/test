6ab4af2db703945ad9f7f9f2bc71b7da8b4f5d4b
jplucy7 <jplucy7@yahoo.co.jp>
2016-04-14 20:06:35 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ $to = $jsonObj->{"result"}[0]->{"content"}->{"from"};
 // テキストで返事をする場合
 $response_format_text = ['contentType'=>1,"toType"=>1,"text"=>"hello"];
 // 画像で返事をする場合
-$response_format_image = ['contentType'=>2,"toType"=>1,'originalContentUrl'=>"http://media-cache-ec0.pinimg.com/600x/ba/0d/97/ba0d97bebb8cb43deb2938ad0707da64.jpg","previewImageUrl"=>"http://media-cache-ec0.pinimg.com/600x/ba/0d/97/ba0d97bebb8cb43deb2938ad0707da64.jpg"];
+$response_format_image = ['contentType'=>2,"toType"=>1,'originalContentUrl'=>"http://img.laughy.jp/8901/default_b2db3a1dddea19ea9300532eee994155.jpg","previewImageUrl"=>"http://img.laughy.jp/8901/default_b2db3a1dddea19ea9300532eee994155.jpg"];
 // 他にも色々ある
 // ....
 
 // toChannelとeventTypeは固定値なので、変更不要。
-$post_data = ["to"=>[$to],"toChannel"=>"1383378250","eventType"=>"138311608800106203","content"=>$response_format_text];
+$post_data = ["to"=>[$to],"toChannel"=>"1383378250","eventType"=>"138311608800106203","content"=>$response_format_image];
 
 $ch = curl_init("https://trialbot-api.line.me/v1/events");
 curl_setopt($ch, CURLOPT_POST, true);
