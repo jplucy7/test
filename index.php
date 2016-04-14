84ae0a14ecb699df1648a3d983b2e7c335e2d621
jplucy7 <jplucy7@yahoo.co.jp>
2016-04-14 20:18:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+/*
 $json_string = file_get_contents('php://input');
 $jsonObj = json_decode($json_string);
 $to = $jsonObj->{"result"}[0]->{"content"}->{"from"};
@@ -28,6 +30,8 @@ curl_setopt($ch, CURLOPT_PROXYPORT, '80');
 curl_setopt($ch, CURLOPT_PROXY, 'http://fixie:ivM1p3nHCezmaWX@velodrome.usefixie.com');
 $result = curl_exec($ch);
 curl_close($ch);
+*/
+
 /*
 
 require_once('LineBot.php');
@@ -47,3 +51,19 @@ $bot->sendText('「%s」デスネ...');
 $stdout= fopen( 'php://stdout', 'w' );
 fwrite( $stdout, "hello world to stdout\n" );
 */
+
+<?php
+require_once('LineBot.php');
+// LINE:チャンネルID
+$CHANNEL_ID = '1462191363';
+// LINE:チャンネルシークレット
+$CHANNEL_SECRET = 'c764f2b4d9782f440505970c3b6513ca';
+// LINE:MID
+$CHANNEL_MID = 'udd46d2d93a3d7bf5a0a1e95436d29680';
+// Bingアカウントキー
+$ACCOUNT_KEY = 'ztEmprmeJkJCzLl40QDbKLZdclPK8H2njHIJ1C9jIvY';
+$bot = new LineBot($CHANNEL_ID, $CHANNEL_SECRET, $CHANNEL_MID);
+// テキスト送信
+$bot->sendText('「%s」デスネ...');
+// 画像送信
+$bot->sendImage($ACCOUNT_KEY);
